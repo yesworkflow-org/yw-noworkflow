88eede7cf4308f113e0af5f5259edd3fc2e20a98
Timothy McPhillips <tmcphillips@absoluteflow.org>
2016-04-25 15:25:42 -0700

    Added second Hello World script variant.

---
+++ examples/02_hello_world/hello_world_2.py
@@ -0,0 +1,16 @@
+# @begin HelloWorld @desc Exercise YW and NW with a classic program.
+# @in provided_greeting
+# @out displayed_greeting @desc Greeting displayed to user.
+
+# @begin print_greeting @desc Greet the program user.
+# @in greeting @as provided_greeting
+# @out displayed_greeting @file stream:stdout
+def print_greeting(greeting):
+    print(greeting)
+# @end print_greeting
+
+# @end HelloWorld
+
+if __name__ == '__main__':
+    greeting = 'Hello World!'
+    print_greeting(greeting)
---
+++ examples/02_hello_world/run_queries.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash -l
+#
+# ./run_queries.sh &> run_queries.txt
+
+xsb --noprompt << END_XSB_STDIN
+
+[yw_extract_facts].
+[yw_model_facts].
+[nw_facts].
+['../rules/yw_rules'].
+['../rules/yw_nw_rules'].
+[queries].
+
+printall(yw_q1_desc(_), yw_q1(_,_)).
+printall(yw_q2_desc(_), yw_q2(_,_)).
+printall(yw_q3_desc(_), yw_q3(_,_,_)).
+printall(yw_q4_desc(_), yw_q4(_,_)).
+
+printall(nw_q1_desc(_), nw_q1(_)).
+printall(nw_q2_desc(_), nw_q2(_,_)).
+
+
+END_XSB_STDIN
