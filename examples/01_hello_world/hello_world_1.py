f739415de7686bbdb27f68f411e64266528384e4
Timothy McPhillips <tmcphillips@absoluteflow.org>
2016-04-25 13:06:56 -0700

    Added output port to YW markup for HelloWorld script.

--- examples/01_hello_world/hello_world_1.py
+++ examples/01_hello_world/hello_world_1.py
@@ -1,6 +1,8 @@
 # @begin HelloWorld @desc Exercise YW and NW with a classic program.
+# @out greeting
 
 # @begin print_greeting @desc Greet the program user.
+# @out greeting @file stream:stdout
 print("Hello world!")
 # @end print_greeting
 
