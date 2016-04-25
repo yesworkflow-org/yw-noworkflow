78264d959ec48a02957be22d9d35c274daf9c1e1
Timothy McPhillips <tmcphillips@absoluteflow.org>
2016-04-25 02:18:32 -0700

    Improved Makefile

--- examples/01_hello_world/run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash -l
-#
-# ./run.sh &> run.txt
-
-script='hello_world_1.py'
-yw graph $script > yw_prospective.gv
-now run -e Tracer $script
-now export -m dependency | grep -v 'environment(' > nw_facts.P
