cdf1e82db9d1ec0e1f6fec7855174c28ffb08b45
Timothy McPhillips <tmcphillips@absoluteflow.org>
2016-04-27 11:57:17 -0700

    Fixed typos in run_queries.sh

--- examples/02_hello_world/run_queries.sh
+++ examples/02_hello_world/run_queries.sh
@@ -10,7 +10,7 @@ xsb --quietload --noprompt --nofeedback --nobanner << END_XSB_STDIN
 ['../rules/yw_rules'].
 ['../rules/yw_nw_rules'].
 
-banner('yw_q1(WorkflowName, Description) % What is the name and description of the workflow implemented by the script?').
+banner('yw_q1(WorkflowName,Description) % What is the name and description of the workflow implemented by the script?').
 [user].
 :- table yw_q1/2.
 yw_q1(WorkflowName,Description) :-
@@ -19,7 +19,7 @@ yw_q1(WorkflowName,Description) :-
 end_of_file.
 printall(yw_q1(_,_)).
 
-banner('(StepName,Description) % What workflow steps comprise the top-level workflow?').
+banner('yw_q2(StepName,Description) % What workflow steps comprise the top-level workflow?').
 [user].
 :- table yw_q2/2.
 yw_q2(StepName,Description) :-
@@ -30,7 +30,7 @@ yw_q2(StepName,Description) :-
 end_of_file.
 printall(yw_q2(_,_)).
 
-banner('(SourceFile, StartLine, EndLine) % Where is the definition of workflow step HelloWorld.print_greeting?').
+banner('yw_q3(SourceFile,StartLine,EndLine) % Where is the definition of workflow step HelloWorld.print_greeting?').
 [user].
 :- table yw_q3/3.
 yw_q3(SourceFile, StartLine, EndLine) :-
@@ -38,7 +38,7 @@ yw_q3(SourceFile, StartLine, EndLine) :-
 end_of_file.
 printall(yw_q3(_,_,_)).
 
-banner('(OutputName, Description) % What are the names and descriptions of any outputs of the script?').
+banner('yw_q4(OutputName,Description) % What are the names and descriptions of any outputs of the script?').
 [user].
 :- table yw_q4/2.
 yw_q4(OutputName, Description) :-
@@ -49,7 +49,7 @@ yw_q4(OutputName, Description) :-
 end_of_file.
 printall(yw_q4(_,_)).
 
-banner('(FunctionName, CallLine) % What functions are called from the top level of the script?').
+banner('nw_q1(FunctionName,CallLine) % What functions are called from the top level of the script?').
 [user].
 :- table nw_q1/1.
 nw_q1(FunctionName) :-
@@ -57,7 +57,7 @@ nw_q1(FunctionName) :-
 end_of_file.
 printall(nw_q1(_)).
 
-banner('(ActivationId, ParameterName, VariableName, Value) % What variable values are passed to print_greeting() from the top of the script?').
+banner('nw_q2(ActivationId,ParameterName,VariableName,Value) % What variable values are passed to print_greeting() from the top of the script?').
 [user].
 :- table nw_q2/4.
 nw_q2(ActivationId, ParameterName, VariableName, Value) :-
@@ -65,7 +65,7 @@ nw_q2(ActivationId, ParameterName, VariableName, Value) :-
 end_of_file.
 printall(nw_q2(_,_,_,_)).
 
-banner('(ActivationId, Parameter, Value) % What literal values are passed to print_greeting() from the top of the script?').
+banner('nw_q3(ActivationId,Parameter,Value) % What literal values are passed to print_greeting() from the top of the script?').
 [user].
 :- table nw_q3/3.
 nw_q3(ActivationId, Parameter, Value) :-
