6878bdf03a31f9b621efec01d85b924e695d075f
Timothy McPhillips <tmcphillips@absoluteflow.org>
2016-05-13 11:16:34 -0700

    Renamed graph rules.

--- graphs/yw_data_graph.sh
+++ graphs/yw_data_graph.sh
@@ -7,37 +7,37 @@ xsb --quietload --noprompt --nofeedback --nobanner << END_XSB_STDIN
 set_prolog_flag(unknown, fail).
 
 [yw_views].
-['../../rules/general_rules'].
 ['../../graphs/graph_rules'].
 
 [user].
 graph() :-
 
-    yw_workflow_script(WorkflowId, WorkflowName, _, _),
+    yw_workflow_script(W, WorkflowName, _, _),
 
-    graph_begin('YW_DATA', WorkflowName, 'TB'),
+    gv_graph('yw_data_view', WorkflowName, 'TB'),
 
-    start_visible_cluster_box('workflow'),
-    data_node_style(),
-    graph_data_in_nodes(WorkflowId),
-    param_node_style(),
-    graph_data_param_nodes(WorkflowId),
-    graph_data_to_data_edges(WorkflowId),
-    end_cluster_box(),
+        gv_cluster('workflow', 'black'),
+            gv_node_style_data(),
+            gv_data_in_nodes(W),
+            gv_node_style_param(),
+            gv_data_param_nodes(W),
+        gv_cluster_end(),
 
-    start_invisible_cluster_box('inflows'),
-    workflow_port_style(),
-    graph_inflow_nodes(WorkflowId),
-    end_cluster_box(),
-    graph_inflow_to_data_edges(WorkflowId),
+        gv_cluster('inflows', 'white'),
+            gv_node_style_workflow_port(),
+            gv_inflow_nodes(W),
+        gv_cluster_end(),
 
-    start_invisible_cluster_box('outflows'),
-    workflow_port_style(),
-    graph_outflow_nodes(WorkflowId),
-    end_cluster_box(),
-    graph_data_to_outflow_edges(WorkflowId),
+        gv_cluster('outflows', 'white'),
+            gv_node_style_workflow_port(),
+            gv_outflow_nodes(W),
+        gv_cluster_end(),
 
-    graph_end().
+        gv_data_to_data_edges(W),
+        gv_inflow_to_data_edges(W),
+        gv_data_to_outflow_edges(W),
+
+    gv_graph_end().
 
 end_of_file.
 
