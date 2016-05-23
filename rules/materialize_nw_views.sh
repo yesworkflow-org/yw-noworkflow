a0546134979afed4c2f8fee60fe818929ff74054
Timothy McPhillips <tmcphillips@absoluteflow.org>
2016-05-23 11:09:53 -0700

    Updated example outputs for revised rules.

--- rules/materialize_nw_views.sh
+++ rules/materialize_nw_views.sh
@@ -10,25 +10,25 @@ xsb --quietload --noprompt --nofeedback --nobanner << END_XSB_STDIN
 
 set_prolog_flag(unknown, fail).
 
-rule_banner('nw_script_activation(Script, Command, ScriptActivation, Docstring).').
+rule_banner('nw_script_activation(Script, Command, ScriptAct, Docstring).').
 printall(nw_script_activation(_,_,_,_)).
 
-rule_banner('nw_function_definition(FunctionId, Name, FirstLine, LastLine, Docstring).').
+rule_banner('nw_function_definition(FuncId, Name, FirstLine, LastLine, Docstring).').
 printall(nw_function_definition(_,_,_,_,_)).
 
-rule_banner('nw_function_activation(ActivationId, FunctionId, FunctionName, Line, CallerActivationId).').
+rule_banner('nw_function_activation(ActId, FuncId, FuncName, Line, CallerActId).').
 printall(nw_function_activation(_,_,_,_,_)).
 
-rule_banner('nw_function_argument(ActivationId, FunctionName, ArgumentId, ArgumentName, Value, VariableName, VariableId)').
-printall(nw_function_argument(_,_,_,_,_,_,_)).
+rule_banner('nw_function_argument(ActId, FuncName, LocalVarId, LocalVarName, Value, Type, CallerVarName, CallerVarId)').
+printall(nw_function_argument(_,_,_,_,_,_,_,_)).
 
-rule_banner('nw_variable_assignment(ActivationId, VariableId, VariableName, Line, Value).').
+rule_banner('nw_variable_assignment(ActId, VarId, VarName, Line, Value).').
 printall(nw_variable_assignment(_,_,_,_,_)).
 
-rule_banner('nw_variable_usage(UsageId, ActivationId, VariableId, VariableName, VariableValue, Line).').
+rule_banner('nw_variable_usage(UsageId, ActId, VariableId, VarName, VarValue, Line).').
 printall(nw_variable_usage(_,_,_,_,_,_)).
 
-rule_banner('nw_variable_dependency(DependencyId, ActivationId, FunctionName, AssignmentLine, DownstreamVarId, DownstreamVarName, UpstreamVarId, UpstreamVarName)').
+rule_banner('nw_variable_dependency(DependencyId, ActId, FuncName, AssignmentLine, DownstreamVarId, DownstreamVarName, UpstreamVarId, UpstreamVarName)').
 printall(nw_variable_dependency(_,_,_,_,_,_,_,_)).
 
 END_XSB_STDIN
--- rules/materialize_yw_nw_views.sh
+++ rules/materialize_yw_nw_views.sh
@@ -7,8 +7,8 @@ xsb --quietload --noprompt --nofeedback --nobanner << END_XSB_STDIN
 [yw_views].
 [nw_views].
 ['../../rules/general_rules'].
-['../../rules/yw_nw_views'].
 ['../../rules/yw_nw_rules'].
+['../../rules/yw_nw_views'].
 
 set_prolog_flag(unknown, fail).
 
@@ -18,8 +18,8 @@ printall(nw_activation_from_yw_step(_,_,_,_,_)).
 rule_banner('nw_activation_into_yw_step(ActivationId, FunctionName, StepId, StepName, ActivationLine, CallerActivationId).').
 printall(nw_activation_into_yw_step(_,_,_,_,_,_)).
 
-rule_banner('nw_variable_for_yw_in_port(VariableId, VariableName, VariableValue, DataId, DataName, PortId, PortName, StepId, StepName).').
-printall(nw_variable_for_yw_in_port(_,_,_,_,_,_,_,_,_)).
+rule_banner('nw_variable_for_yw_in_port(VariableId, VariableName, VariableValue, DataId, DataName, Type, PortId, PortName, StepId, StepName).').
+printall(nw_variable_for_yw_in_port(_,_,_,_,_,_,_,_,_,_)).
 
 rule_banner('nw_variable_for_yw_out_port(VariableId, VariableName, VariableValue, DataId, DataName, PortId, PortName, StepId, StepName).').
 printall(nw_variable_for_yw_out_port(_,_,_,_,_,_,_,_,_)).
