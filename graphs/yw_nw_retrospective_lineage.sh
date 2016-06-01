9b23d2fe62c38731103d0c6b9600522536a9cf48
Timothy McPhillips <tmcphillips@absoluteflow.org>
2016-05-31 18:42:32 -0700

    Fixed bug in data_values_upstream_of_data_product/9.

--- graphs/yw_nw_retrospective_lineage.sh
+++ graphs/yw_nw_retrospective_lineage.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash -l
 
-DataProductName=$1
-DataProductValue=$2
+DataName=$1
+DataValue=$2
 
 xsb --quietload --noprompt --nofeedback --nobanner << END_XSB_STDIN
 
@@ -21,37 +21,36 @@ xsb --quietload --noprompt --nofeedback --nobanner << END_XSB_STDIN
 
 graph() :-
 
-    DataProductName = '$DataProductName',
-    DataProductValue = '$DataProductValue',
+    V = '$DataValue',
     yw_workflow_script(W, WorkflowName, _, _),
-    yw_data(DataProductId, DataProductName, W, _),
+    yw_data(D, '$DataName', W, _),
 
     gv_graph('yw_data_view', WorkflowName, 'TB'),
         gv_cluster('workflow', 'black'),
             gv_nodestyle__atomic_step(),
-            gv_nodes__atomic_steps__upstream_of_data(W, DataProductId),
+            gv_nodes__atomic_steps__upstream_of_data(W,D),
             gv_nodestyle__subworkflow(),
-            gv_nodes__subworkflows__upstream_of_data(W, DataProductId),
+            gv_nodes__subworkflows__upstream_of_data(W,D),
             gv_node_style__data(),
-            gv_nodes__data_values__upstream_of_data_product(W, DataProductId, DataProductName, DataProductValue),
+            gv_nodes__data_values__upstream_of_data_product(W,D,V),
             %gv_node_style__param(),
-            %gv_nodes__param_values__upstream_of_data_product(W, DataProductId, DataProductName, DataProductValue),
+            %gv_nodes__param_values__upstream_of_data_product(W,D,V),
         gv_cluster_end(),
 
         gv_cluster('inflows', 'white'),
             gv_node_style__workflow_port(),
-            gv_nodes__inflows__upstream_of_data(W, DataProductId),
+            gv_nodes__inflows__upstream_of_data(W,D),
         gv_cluster_end(),
 
         gv_cluster('outflows', 'white'),
             gv_node_style__workflow_port(),
-            gv_node__outflow_for_data(W, DataProductId),
+            gv_node__outflow_for_data(W,D),
         gv_cluster_end(),
 
-        gv_edges__data_to_step__upstream_of_data(W, DataProductId),
-        gv_edges__step_to_data__upstream_of_data(W, DataProductId),
-        gv_edges__inflow_to_data__upstream_of_data(W, DataProductId),
-        gv_edges__data_to_outflow__upstream_of_data(W, DataProductId),
+        gv_edges__data_to_step__upstream_of_data(W, D),
+        gv_edges__step_to_data__upstream_of_data(W, D),
+        gv_edges__inflow_to_data__upstream_of_data(W, D),
+        gv_edges__data_to_outflow__upstream_of_data(W, D),
 
     gv_graph_end().
 
